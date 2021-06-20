e857217d0aa21d0020a50b2b64a68f378e578c4d
gedesurya125 <gedesurya125@gmail.com>
2021-06-20 10:18:13 +0800

    adding nav button

--- js/index.js
+++ js/index.js
@@ -2,11 +2,12 @@ AOS.init();
 $().ready(function () {
   $(window).scroll(function (event) {
     let scroll = this.scrollY;
-    console.log(scroll);
+    //console.log(scroll);
     if(scroll > $("header").height()){
       $("header").addClass("show");
       $("#welcome-section").addClass("full")
       $("header > span, header > span > img").addClass("minimize");
+
     }else{
       $("header").removeClass("show");
       $("#welcome-section").removeClass("full");
@@ -16,6 +17,20 @@ $().ready(function () {
 
 //fade project
 let contact = document.getElementById("contact");
-console.log(contact.getBoundingClientRect().top, contact.scrollLeft);
-
+//console.log(contact.getBoundingClientRect().top, contact.scrollLeft);
 });
+function openNavBar(event){
+console.log("i am clicked");
+const menuIcon = document.querySelector(".icon-group");
+
+const navBar = document.querySelector("#navbar");
+if( navBar.style.maxHeight === "" ){
+  const navHeight = navBar.scrollHeight;
+  navBar.style.maxHeight = navHeight.toString()+'px';
+  menuIcon.style.top ="-30px";
+}else{
+  navBar.style.maxHeight = "";
+  menuIcon.style.top ="0px";
+
+}
+}
